c8a8a1ed83cbc62b38c0ca62182372dd7484c5a6
Yi-Chun Lin <jyichun.lin@gmail.com>
2015-07-22 08:27:23 -0400

    portfolio text, contatct

--- sendemail.php
+++ sendemail.php
@@ -3,7 +3,7 @@ $name       = @trim(stripslashes($_POST['name']));
 $from       = @trim(stripslashes($_POST['email']));
 $subject    = @trim(stripslashes($_POST['subject']));
 $message    = @trim(stripslashes($_POST['message']));
-$to   		= 'jyichun.lin@gmail.com';//replace with your email
+$to   		= 'jean@yichunlin.com';//replace with your email
 
 $headers   = array();
 $headers[] = "MIME-Version: 1.0";
